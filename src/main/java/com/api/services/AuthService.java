77fb0546709ad817b39df2ce95da00e3d5a32002
pratiksha <jadhav.pratiksha001@gmail.com>
2026-02-02 18:09:40 +0530

    second commit

--- src/main/java/com/api/services/AuthService.java
+++ src/main/java/com/api/services/AuthService.java
@@ -1,4 +1,13 @@
 package com.api.services;
 
-public class AuthService {
+import io.restassured.response.Response;
+
+public class AuthService extends BaseService{
+
+    private static final String BASE_PATH = "/api/auth/";
+
+    public Response login(String payload){
+        return postRequest(payload, BASE_PATH+"login");
+
+    }
 }
